cab77441b8d4afe9c700d843be6725167cb5b31c
Irawan <irawan@live.com.au>
2026-05-14 02:20:07 +0000

    make the isProd change on front

--- projects/fuel-calculator/astro.config.mjs
+++ projects/fuel-calculator/astro.config.mjs
@@ -1,6 +1,9 @@
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
+
+const isProd = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
   site: 'https://jamirawan.github.io',
-  base: '/tent-pg/projects/fuel-calculator',
+  base: isProd ? '/tent-pg/projects/fuel-calculator' : '/',
 });
