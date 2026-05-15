8a9b82aa07af4694ad25ebcfbc4febdc71fa6e26
Irawan <irawan@live.com.au>
2026-05-15 03:35:43 +0000

    fix root

--- projects/fuel-calculator/astro.config.mjs
+++ projects/fuel-calculator/astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 const isProd = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
-  site: 'https://jamirawan.github.io',
-  base: isProd ? '/tent-pg/projects/fuel-calculator' : '/',
+  site: 'https://jamirawan.github.io/tent-pg',
+  base: isProd ? '/tent-pg/' : '/',
 });
